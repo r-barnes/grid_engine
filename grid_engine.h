c22030b561d551476b7929f5adb7a075ce6361fb
Richard Barnes <rbarnes@umn.edu>
2012-05-18 02:07:24 +0000

    Partial iterator implementation
    git-svn-id: file:///home/rick/.svn/grid_engine@6 62c8f9c8-2513-441c-ac89-7743ec178e1e

--- grid_engine.h
+++ grid_engine.h
@@ -1,6 +1,9 @@
 #include <vector>
+template <class T> class grid_engine_iterator;
+
 template <class T>
 class grid_engine {
+	friend class grid_engine_iterator<T>;
 	private:
 		std::vector< std::vector<T> > data;
 	public:
@@ -8,19 +11,32 @@ class grid_engine {
 		typedef T		*pointer;
 		typedef const T	*const_pointer;
 		typedef T		&reference;
-		typedef const T	&const_reference;		
+		typedef const T	&const_reference;
+		typedef int		size_type;
+		typedef grid_engine_iterator<T> iterator;
 		reference operator()(int x, int y)
 			{return data[y][x];}
 		const_reference operator()(int x, int y) const
 			{return data[y][x];}
 		void resize(int Gwidth, int Gheight, bool preserve=false);
-		int width() const
+		size_type width() const
 			{return data[0].size();}
-		int height() const
+		size_type height() const
 			{return data.size();}
 		grid_engine();
 		grid_engine(int width, int height);
-		void clear() { data.clear(); }
+		void clear()
+			{data.clear();}
+		reference front()
+			{return data[0][0];}
+		reference back()
+			{return data[height()-1][width()-1];}
+		const_reference front() const
+			{return data[0][0];}
+		const_reference back() const
+			{return data[height()-1][width()-1];}
+		iterator begin() { return iterator(*this, 0, 0); }
+		iterator end() { return iterator(*this, width()-1, height()-1); }
 };
 
 template <class T>
@@ -37,6 +53,26 @@ void grid_engine<T>::resize(int Gwidth, int Gheight, bool preserve){
 	for(int i=0;i<Gheight;i++)
 		data[i].resize(Gwidth);
 }
+
+template <class T>
+class grid_engine_iterator{
+	private:
+		grid_engine<T>	&my_ge;
+		int x,y;
+	public:
+		grid_engine_iterator( grid_engine<T> &ge, int x0, int y0 ){
+			my_ge=ge;
+			x=x0;
+			y=y0;
+		}
+		bool operator==(grid_engine<T>::iterator &other){
+			return (other.my_ge==my_ge && other.x==x && other.y==y);
+		}
+		bool operator!=(grid_engine<T>::iterator &other){
+			return !(operator==(other));
+		}
+};
+
 
 
 template <class T>

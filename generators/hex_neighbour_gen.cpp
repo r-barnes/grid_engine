691e856c86d59eeea18c486444f849fb6396c67e
Richard Barnes <rbarnes@umn.edu>
2013-04-09 11:32:59 -0500

    Eliminated hex references. Added d8 and d4 systems

--- generators/hex_neighbour_gen.cpp
+++ generators/hex_neighbour_gen.cpp
@@ -7,11 +7,7 @@
 #include <set>
 using namespace std;
 
-#define NF(y,column) (((column%2)==0)?(y):-(y))
-
-//Even columns. Multiply y-coordinate by -1 to make this odd columns
-int dx_hex[6]={ 0, -1, 1, 0, -1,  1};
-int dy_hex[6]={-1,  0, 0, 1, -1, -1};
+#define NF(y,column) (((column%2)==0)?(y):-(y)) //Used for hex
 
 class grid_cell{
   public:
@@ -22,59 +18,97 @@ class grid_cell{
 };
 
 int main(int argc, char **argv){
-  if(argc!=2){
-    cout<<argv[0]<<" <NUMBER OF RINGS>"<<endl;
+  if(argc!=3){
+    cout<<argv[0]<<" <hex/d8/d4> <NUMBER OF RINGS>"<<endl;
     return -1;
   }
 
-  int maxrings=atoi(argv[1]);
+  int maxrings=atoi(argv[2]);
 
-	queue<grid_cell> hex_edge;
+	vector<int> dx,dy;
+	if(argv[1][0]=='h'){
+    //Even columns. Multiply y-coordinate by -1 to make this odd columns
+		//int dx_hex[6]={ 0, -1, 1, 0, -1,  1};
+		//int dy_hex[6]={-1,  0, 0, 1, -1, -1};
+		dx.push_back( 0); 		dy.push_back(-1);
+		dx.push_back(-1); 		dy.push_back( 0);
+		dx.push_back( 1); 		dy.push_back( 0);
+		dx.push_back( 0); 		dy.push_back( 1);
+		dx.push_back(-1); 		dy.push_back(-1);
+		dx.push_back( 1); 		dy.push_back(-1);
+	} else if (argv[1][1]=='8') {
+		dx.push_back(-1);			dy.push_back(-1);
+		dx.push_back( 0);			dy.push_back(-1);
+		dx.push_back( 1);			dy.push_back(-1);
+		dx.push_back(-1);			dy.push_back( 0);
+		dx.push_back( 1);			dy.push_back( 0);
+		dx.push_back(-1);			dy.push_back( 1);
+		dx.push_back( 0);			dy.push_back( 1);
+		dx.push_back( 1);			dy.push_back( 1);
+	} else if (argv[1][1]=='4') {
+		dx.push_back( 0);			dy.push_back(-1);
+		dx.push_back(-1);			dy.push_back( 0);
+		dx.push_back( 1);			dy.push_back( 0);
+		dx.push_back( 0);			dy.push_back( 1);
+	}
+
+	queue<grid_cell> edge;
   vector<int> begins, xs, ys;
   set<grid_cell> used;
   int ring=0;
 
-	hex_edge.push(grid_cell(999999,999999));
-	hex_edge.push(grid_cell(0,0));
+	edge.push(grid_cell(999999,999999));
+	edge.push(grid_cell(0,0));
 
   begins.push_back(0);
   xs.push_back(0);
   ys.push_back(0);
 
 	while(true){
-		grid_cell c=hex_edge.front();
-		hex_edge.pop();
+		grid_cell c=edge.front();
+		edge.pop();
 
 		if(c.x==999999){
       ring++;
       if(ring>=maxrings)
         break;
       begins.push_back(xs.size());
-			hex_edge.push(grid_cell(999999,999999));
+			edge.push(grid_cell(999999,999999));
 			continue;
 		}
 
-		for(int n=0;n<6;n++){	//Look through neighbours
-			if (!used.count(grid_cell(c.x+dx_hex[n],c.y+NF(dy_hex[n],c.x)))) {
-        used.insert(grid_cell(c.x+dx_hex[n],c.y+NF(dy_hex[n],c.x)));
-        xs.push_back(c.x+dx_hex[n]);
-        ys.push_back(c.y+NF(dy_hex[n],c.x));
-				hex_edge.push(grid_cell(c.x+dx_hex[n],c.y+NF(dy_hex[n],c.x)));
+		if(argv[1][0]=='h'){
+			for(int n=0;n<dx.size();n++){	//Look through neighbours
+				if (!used.count(grid_cell(c.x+dx[n],c.y+NF(dy[n],c.x)))) {
+		      used.insert(grid_cell(c.x+dx[n],c.y+NF(dy[n],c.x)));
+		      xs.push_back(c.x+dx[n]);
+		      ys.push_back(c.y+NF(dy[n],c.x));
+					edge.push(grid_cell(c.x+dx[n],c.y+NF(dy[n],c.x)));
+				}
+			}
+		} else {
+			for(int n=0;n<dx.size();n++){	//Look through neighbours
+				if (!used.count(grid_cell(c.x+dx[n],c.y+dy[n]))) {
+		      used.insert(grid_cell(c.x+dx[n],c.y+dy[n]));
+		      xs.push_back(c.x+dx[n]);
+		      ys.push_back(c.y+dy[n]);
+					edge.push(grid_cell(c.x+dx[n],c.y+dy[n]));
+				}
 			}
 		}
 	}
 
-  cout<<"hex_begins[]={"<<begins[0];
+  cout<<argv[1]<<"_begins[]={"<<begins[0];
   for(int i=1;i<begins.size();++i)
     cout<<", "<<begins[i];
   cout<<"};"<<endl<<endl;
 
-  cout<<"hex_dx[]={"<<xs[0];
+  cout<<argv[1]<<"_dx[]={"<<xs[0];
   for(int i=1;i<xs.size();++i)
     cout<<", "<<xs[i];
   cout<<"};"<<endl<<endl;
 
-  cout<<"hex_dy[]={"<<ys[0];
+  cout<<argv[1]<<"_dy[]={"<<ys[0];
   for(int i=1;i<ys.size();++i)
     cout<<", "<<ys[i];
   cout<<"};"<<endl<<endl;

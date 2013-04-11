5822a7aa7d1666b6cf4e5709f3b63e4648b048da
Richard Barnes <rbarnes@umn.edu>
2013-04-11 15:00:31 -0500

    Added a note about the kind of output this produces

--- generators/neighbour_gen.cpp
+++ generators/neighbour_gen.cpp
@@ -28,6 +28,7 @@ int main(int argc, char **argv){
 	vector<int> dx,dy;
 	if(argv[1][0]=='H'){
     //Even columns. Multiply y-coordinate by -1 to make this odd columns
+    //Truth: In the resulting output odd columns are shifted a half-cell down with respect to even columns
 		//int dx_hex[6]={ 0, -1, 1, 0, -1,  1};
 		//int dy_hex[6]={-1,  0, 0, 1, -1, -1};
 		dx.push_back( 0); 		dy.push_back(-1);

02067484630afc5925c6425f989120f275ae69b4
Richard Barnes <rbarnes@umn.edu>
2013-04-13 00:10:29 -0500

    Updated for increased flexibility and even/odd hex neighbourhoods

--- generators/plotter.py
+++ generators/plotter.py
@@ -12,15 +12,15 @@ class GridDrawer(Frame):
     canvas.pack(expand=YES, fill=BOTH)
     self.canvas = canvas
     self.size=20
-    self.xoffset=0
+    self.xoffset=200
     self.yoffset=200
 
   def refresh(self):
     self.canvas.configure(scrollregion=self.canvas.bbox("all"))
 
-  def DrawSquare(self, orgx, orgy, filler):
-    orgx=orgx*self.size+self.xoffset
-    orgy=orgy*self.size+self.yoffset
+  def DrawSquare(self, x, y, filler):
+    orgx=x*self.size+self.xoffset
+    orgy=y*self.size+self.yoffset
     self.hex = self.canvas.create_polygon(
       orgx, orgy,
       orgx+self.size, orgy,
@@ -30,11 +30,13 @@ class GridDrawer(Frame):
       outline="black", fill=filler
     )
 
-  def DrawHex(self, orgx, orgy, filler):
+    self.canvas.create_text(orgx+self.size/2, orgy+self.size/2, text=str(x)+","+str(y))
+
+  def DrawHex(self, x, y, filler):
     oneseg = self.size/4
     twoseg = oneseg*2  # same as size/2
-    orgy=orgy*self.size+(orgx%2)*self.size/2 + self.yoffset
-    orgx=orgx*self.size*3/4.                 + self.xoffset
+    orgy=y*self.size+(x%2)*self.size/2 + self.yoffset
+    orgx=x*self.size*3/4.              + self.xoffset
 
     # Create a new one
     self.hex = self.canvas.create_polygon( 
@@ -47,15 +49,23 @@ class GridDrawer(Frame):
     orgx, orgy + twoseg,
     outline="black",fill=filler)
 
+    self.canvas.create_text(orgx+twoseg, orgy+twoseg, text=str(x)+","+str(y))
+
+def toroid(i,imax):
+  if i>=0:
+    return i%imax
+  else:
+    return imax-((-i)%imax)
+
 def main():
   h=GridDrawer()
 
   if len(sys.argv)!=3:
-    print "%s <hex/d4/d8> <coordinates file>" % (sys.argv[0])
+    print "%s <HexEven/HexOdd/D4/D8> <coordinates file>" % (sys.argv[0])
     sys.exit(-1)
 
   drawer=None
-  if sys.argv[1]=='Hex':
+  if sys.argv[1]=='HexEven' or sys.argv[1]=='HexOdd':
     drawer=h.DrawHex
   elif sys.argv[1]=='D4' or sys.argv[1]=='D8':
     drawer=h.DrawSquare
@@ -76,14 +86,17 @@ def main():
     if l.startswith("int " + sys.argv[1] + "::dy"):
       dy=[int(i) for i in l[l.index('{')+1:l.index('}')].split(',')]
 
-  for x in range(-30,30):
-    for y in range(-30,30):
+  for x in range(0,6):
+    for y in range(0,4):
       drawer(x,y,"white")
 
-  drawer(0,0,"blue")
+  x0=2
+  y0=0
 
-  for i in range(begins[2],begins[4]):
-    drawer(dx[i],dy[i],"red")
+  drawer(x0,y0,"blue")
+
+  for i in range(begins[3],begins[4]):
+    drawer(x0+dx[i],y0+dy[i],"red")
 
   h.refresh()
 
